dd2947c4afd08e802a39cf8cf7e52a002dc7c959
peterjbone <peterjbone41@gmail.com>
2024-04-23 10:46:51 -0500

    Primer commit

---
+++ backend_sanity/schemaTypes/index.js
@@ -0,0 +1,19 @@
+import abouts from '../schemas/abouts'
+import brands from '../schemas/brands'
+import contact from '../schemas/contact'
+import experiences from '../schemas/experiences'
+import skills from '../schemas/skills'
+import testimonials from '../schemas/testimonials'
+import workExperience from '../schemas/workExperience'
+import works from '../schemas/works'
+
+export const schemaTypes = [
+  abouts,
+  brands,
+  contact,
+  experiences,
+  skills,
+  testimonials,
+  workExperience,
+  works,
+]
