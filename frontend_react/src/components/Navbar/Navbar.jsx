e4b0d9c3af28c7eac8ec710b9b376b47d00ad712
peterjbone <peterjbone41@gmail.com>
2024-10-27 21:53:38 -0500

    Updating portfolio

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,26 @@ import "./Navbar.scss";
 //import { motion } from "framer-motion";
 import { v4 as uuidv4 } from "uuid";
 import LanguageBtns from "../LanguageBtn/LanguageBtn.jsx";
+import { useLanguageStore } from "../../stores/languageStore.js";
 
 const Navbar = () => {
+	const language = useLanguageStore((state) => state.language);
 	//const [toggle, setToggle] = useState(false);
-	const sections = [
+
+	const sectionsESP = [
 		{ name: "Inicio", link: "home" },
 		{ name: "Sobre mí", link: "about" },
 		{ name: "Proyectos", link: "work" },
 		{ name: "Habilidades", link: "skills" },
 		{ name: "Contacto", link: "contact" }
+	];
+
+	const sectionsENG = [
+		{ name: "Home", link: "home" },
+		{ name: "About", link: "about" },
+		{ name: "Projects", link: "work" },
+		{ name: "Skills", link: "skills" },
+		{ name: "Contact", link: "contact" }
 	];
 
 	return (
@@ -32,13 +43,23 @@ const Navbar = () => {
 				</a>
 			</div>
 
-			<ul className="app__navbar-links">
-				{sections.map((item) => (
-					<li key={uuidv4()} className="app__flex p-text">
-						<a href={`#${item.link}`}>{item.name}</a>
-					</li>
-				))}
-			</ul>
+			{language === "es" ? (
+				<ul className="app__navbar-links">
+					{sectionsESP.map((item) => (
+						<li key={uuidv4()} className="app__flex p-text">
+							<a href={`#${item.link}`}>{item.name}</a>
+						</li>
+					))}
+				</ul>
+			) : (
+				<ul className="app__navbar-links">
+					{sectionsENG.map((item) => (
+						<li key={uuidv4()} className="app__flex p-text">
+							<a href={`#${item.link}`}>{item.name}</a>
+						</li>
+					))}
+				</ul>
+			)}
 
 			{<LanguageBtns />}
 		</nav>
